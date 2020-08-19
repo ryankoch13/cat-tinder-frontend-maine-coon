55dec7b25f41de62ff9fa8b15b0d840a3d014d60
jlmooreiii <jloftonmooreiii@gmail.com>
2020-08-19 10:43:56 -0700

    CatIndex side of form complete

--- cat-tinder-frontend/src/pages/CatNew.js
+++ cat-tinder-frontend/src/pages/CatNew.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import {Redirect} from 'react-router-dom';
+import {Form, FormGroup, Input, Label, Button} from 'reactstrap';
 
 
 export class CatsHome extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            form: {
+                name: "",
+                age: "",
+                enjoys: ""
+            }
+        }      
+    }
+    
+    handleChange = (e) => {
+        let {form} = this.state;
+        form[e.taget.name] = e.target.value;
+        this.setState( {form: form} )
+    }
     render() {
         return (
             <div>
                 <Header />
+                <Form>
+                    <FormGroup>
+                        <Label>Name</Label>
+                        <Input 
+                            type = "text"
+                            name = "name"
+                            value = {this.state.form.name} 
+                            onChange = {this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Age</Label>
+                        <Input
+                            type = "number"
+                            name = "age"
+                            value = {this.state.form.age}
+                            onChange = {this.handleChange} 
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Enjoys</Label>
+                        <Input
+                            type = "text"
+                            name = "enjoys"
+                            value = {this.state.form.enjoys} 
+                            onChange = {this.handleChange}
+                        />
+                    </FormGroup>
+                </Form>
                 
 
                 <Footer />
